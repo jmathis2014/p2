49d731b1a6f6c7b8c2d15b9635e10442c0bf5ce1
James Mathis <james_mathis@harvard.edu>
2015-09-23 22:16:38 -0400

    Header routines

---
+++ header.php
@@ -0,0 +1,8 @@
+<head>
+  <meta charset="utf-8">
+  <title>Project 2</title>
+  <meta name="description" content="Project 2">
+  <meta name="author" content="MMWriting">
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+</head>
+
