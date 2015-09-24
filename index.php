8ead3f3cf59b2afb4e39a8da05970a242a048437
James Mathis <james_mathis@harvard.edu>
2015-09-23 22:12:37 -0400

    Home page

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php include('header.php'); php?>
+  <body>
+    <?php include('content.php'); php?>
+  </body>
+  <script src="js/scripts.js"></script>
+</html>
+
